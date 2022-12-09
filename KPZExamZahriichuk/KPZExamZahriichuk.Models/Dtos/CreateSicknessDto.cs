a38f1c6a858cc9c51c8c386de2c0f16fa87f01d4
vzahriichuk <vzahriichuk@binariks.com>
2022-12-09 16:23:20 +0200

    Add Backend project

---
+++ KPZExamZahriichuk/KPZExamZahriichuk.Models/Dtos/CreateSicknessDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KPZExamZahriichuk.Models.Dtos;
+
+public class CreateSicknessDto
+{
+    public string Description { get; set; }
+    public string Date { get; set; }
+    public int PatientId { get; set; }
+    public int DoctorId { get; set; }
+
+}
